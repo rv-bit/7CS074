08de38e6949f656d94402d29e5e9aaff3edcaa55
rv-bit <robertvilcu89@gmail.com>
2026-01-18 17:48:13 +0000

    chore: deleted 'automatic_make_model_selection' and just use the code split up inside the regression notebook as this 'automatic_make_model_selection' will only be used for regression

--- src/models.py
+++ src/models.py
@@ -1,11 +1,7 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.cluster import KMeans
-
-from sklearn.model_selection import train_test_split
-
-import src.evaluation as evaluation
-import src.features as features
+from sklearn.tree import DecisionTreeClassifier
 
 import global_vars
 
@@ -33,7 +29,8 @@ def get_classification_candidate_models(random_state=42):
             n_estimators=100,
             max_depth=8,
             random_state=random_state
-        )
+        ),
+        "decision_tree": DecisionTreeClassifier(random_state=random_state, class_weight="balanced"),
     }
 
 def get_clustering_candidate_models(clusters=3, random_state=42):
@@ -58,85 +55,3 @@ def get_type_models(type_models):
 
         case _:
             raise ValueError("Type model isn't found in the list")
-
-def automatic_make_model_selection(
-    df,
-    target_col,
-
-    type_models='regression',
-    
-    # Tunning
-    min_samples_per_make=200,
-    cv_splits=5
-):
-    candidate_models = get_type_models(type_models)
-    
-    all_models = {}
-    trained_models_per_make = {}
-
-    X_global, global_encoder = features.engineer_features(
-        df,
-        global_vars.CATEGORICAL_FEATURES_GLOBAL
-    )
-    Y_global = df[target_col]
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X_global, Y_global, test_size=0.2, random_state=42
-    )
-
-    best_name, best_model, cv_scores = evaluation.select_best_model_cv(
-        X_train, y_train, candidate_models, cv_splits
-    )
-    
-    global_predictions = best_model.predict(X_test)
-    global_metrics = evaluation.regression_metrics(y_test, global_predictions)
-
-    all_models["GLOBAL"] = {
-        "best_model": best_name,
-        "model": best_model,
-        "cv_scores": cv_scores,
-        "metrics": global_metrics
-    }
-
-    # Per-make models
-    # If the length of each dataframe of group is lower than the minimum sample variable, we shall pass and not create a 'per-make' model
-    # This is to make sure that algorithms like 'Random Forrest' gets trained on larger sets of data, as indented, if the condition is true, the make would fallback into the global model above.
-    for make, group_df in df.groupby("make"):
-        if len(group_df) < min_samples_per_make:
-            continue
-        
-        X, encoder = features.engineer_features(
-            group_df, 
-            global_vars.CATEGORICAL_FEATURES_PER_MAKE
-        )
-        Y = group_df[target_col]
-
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, Y, test_size=0.2, random_state=42
-        )
-
-        best_name, best_model, cv_scores = evaluation.select_best_model_cv(
-            X_train, y_train, candidate_models, cv_splits
-        )
-
-        predictions = best_model.predict(X_test)
-        metrics = evaluation.regression_metrics(y_test, predictions)
-
-        all_models[make] = {
-            "best_model": best_name,
-            "model": best_model,
-            "cv_scores": cv_scores,
-            "metrics": metrics
-        }
-
-        trained_models_per_make[make] = {
-            "model": best_model,
-            "encoder": encoder,
-            "feature_names": X_train.columns,
-            "X_test": X_test,
-            "y_test": y_test,
-            "predictions": predictions,
-            "metrics": metrics
-        }
-
-    return all_models, trained_models_per_make
