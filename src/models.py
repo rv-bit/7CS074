e1d468933080384191b56d0d13f13ff754c6910d
rv-bit <robertvilcu89@gmail.com>
2026-01-18 10:25:44 +0000

    chore: add candidate_models functions to use multiple models to test against best model, engineer features directly in the 'automatic_make_model_selection' function and not in notebook

--- src/models.py
+++ src/models.py
@@ -3,95 +3,142 @@ from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.cluster import KMeans
 
 from sklearn.model_selection import train_test_split
-from src.evaluation import regression_metrics
 
-def get_regression_model(model_name: str):
-    if model_name == "linear":
-        return LinearRegression()
-    elif model_name == "random_forest":
-        return RandomForestRegressor(
-            n_estimators=200,
-            max_depth=10,
-            random_state=42
+import src.evaluation as evaluation
+import src.features as features
+
+import global_vars
+
+def get_regression_candidate_models(random_state=42):
+    """
+        Returns a list of different regression models, starting with a base model, and hyper tunning others, this can allow to find which is the best model in the case of regression for our current dataset of used cars
+    """
+    return {
+        "linear": LinearRegression(),
+        "random_forest": RandomForestRegressor(
+            n_estimators=300,
+            max_depth=None,
+            min_samples_leaf=5,
+            random_state=random_state,
+            n_jobs=5
         )
-    else:
-        raise ValueError("Unknown model type")
+    }
 
-def get_classification_model(model_name: str):
-    if model_name == "random_forest":
-        return RandomForestClassifier(
+def get_classification_candidate_models(random_state=42):
+    """
+        Returns a list of different classification models, starting with a base model, and hyper tunning others, this can allow to find which is the best model in the case of classification for our current dataset of used cars
+    """
+    return {
+        "random_forrest": RandomForestClassifier(
             n_estimators=100,
             max_depth=8,
-            random_state=42
-    )
-    else:
-        raise ValueError("Unknown model type")
-    
+            random_state=random_state
+        )
+    }
 
-def get_clustering_model(k: int = 3):
-    return KMeans(n_clusters=k, random_state=42)
+def get_clustering_candidate_models(clusters=3, random_state=42):
+    """
+        Returns a list of different clustering models, starting with a base model, and hyper tunning others, this can allow to find which is the best model in the case of clustering for our current dataset of used cars
+    """
+    return {
+        'k-means': KMeans(n_clusters=clusters, random_state=random_state)
+    }
 
+def get_type_models(type_models):
+    """
+        Returns a the candidate models base on the argument passed, if not found raise error
+    """
+    match type_models:
+        case 'regression':
+            return get_regression_candidate_models()
+        case 'classification':
+            return get_classification_candidate_models()
+        case 'clustering':
+            return get_clustering_candidate_models(),
 
-def train_model(X, y, model_type="random_forest"):
-    model = get_regression_model(model_type)
-    model.fit(X, y)
-    return model
-
+        case _:
+            raise ValueError("Type model isn't found in the list")
 
 def automatic_make_model_selection(
     df,
-    feature_cols,
     target_col,
-    model_type="random_forest",
-    min_samples_per_make=200
+
+    type_models='regression',
+    
+    # Tunning
+    min_samples_per_make=200,
+    cv_splits=5
 ):
+    candidate_models = get_type_models(type_models)
+    
     all_models = {}
     trained_models_per_make = {}
 
     # Global model
-    X_global = df[feature_cols]
+    # Gets trained on all data, all columns, very good for averaging the whole dataset no matter the model / make of vehicle
+    features.fit_feature_encoder(
+        df,
+        categorical_cols=global_vars.CATEGORICAL_FEATURES_GLOBAL
+    )
+    X_global = features.engineer_features(df)
     y_global = df[target_col]
 
     X_train, X_test, y_train, y_test = train_test_split(
         X_global, y_global, test_size=0.2, random_state=42
     )
 
-    global_model = train_model(X_train, y_train, model_type=model_type)
-    global_predictions = global_model.predict(X_test)
-    global_metrics = regression_metrics(y_test, global_predictions)
+    best_name, best_model, cv_scores = evaluation.select_best_model_cv(
+        X_train, y_train, candidate_models, cv_splits
+    )
+    
+    global_predictions = best_model.predict(X_test)
+    global_metrics = evaluation.regression_metrics(y_test, global_predictions)
 
-    all_models['GLOBAL'] = {
-        'model': global_model,
-        'metrics': global_metrics
+    all_models["GLOBAL"] = {
+        "best_model": best_name,
+        "model": best_model,
+        "cv_scores": cv_scores,
+        "metrics": global_metrics
     }
 
     # Per-make models
-    for make, group in df.groupby("make"):
-        if len(group) < min_samples_per_make:
+    # If the length of each dataframe of group is lower than the minimum sample variable, we shall pass and not create a 'per-make' model
+    # This is to make sure that algorithms like 'Random Forrest' gets trained on larger sets of data, as indented, if the condition is true, the make would fallback into the global model above.
+    for make, group_df in df.groupby("make"):
+        if len(group_df) < min_samples_per_make:
             continue
 
-        X = group[feature_cols]
-        y = group[target_col]
+        features.fit_feature_encoder(
+            group_df,
+            categorical_cols=global_vars.CATEGORICAL_FEATURES_PER_MAKE
+        )
+        X = features.engineer_features(group_df)
+        Y = group_df[target_col]
 
         X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.2, random_state=42
+            X, Y, test_size=0.2, random_state=42
         )
 
-        model = train_model(X_train, y_train)
-        predictions = model.predict(X_test)
-        metrics = regression_metrics(y_test, predictions)
+        best_name, best_model, cv_scores = evaluation.select_best_model_cv(
+            X_train, y_train, candidate_models, cv_splits
+        )
+
+        predictions = best_model.predict(X_test)
+        metrics = evaluation.regression_metrics(y_test, predictions)
 
         all_models[make] = {
-            'model': model,
-            'metrics': metrics
+            "best_model": best_name,
+            "model": best_model,
+            "cv_scores": cv_scores,
+            "metrics": metrics
         }
-        
+
         trained_models_per_make[make] = {
-            'model': model,
-            'X_test': X_test,
-            'y_test': y_test,
-            'metrics': metrics,
-            'predictions': predictions
+            "model": best_model,
+            "X_test": X_test,
+            "y_test": y_test,
+            "predictions": predictions,
+            "metrics": metrics
         }
 
     return all_models, trained_models_per_make
